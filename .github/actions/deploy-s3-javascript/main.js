09052e6450ea3889afc2d7ea910c335be0af3d55
Gaurav Luthra <gluthra81@gmail.com>
2026-05-11 22:28:51 +0530

    add first custom JS action

---
+++ .github/actions/deploy-s3-javascript/main.js
@@ -0,0 +1,10 @@
+const core =  require('@actions/core');
+const github = require('@actions/github');
+const exec = require('@actions/exec');
+
+
+function hello(){
+    core.notice("Hello from my custom javascript action!");
+}
+
+hello()
