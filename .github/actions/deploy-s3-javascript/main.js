e291d9ede6cbea8929279c008fbc26fcdb67affe
Gaurav Luthra <gluthra81@gmail.com>
2026-05-13 12:34:55 +0530

    add custom js action with R2

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,10 +1,47 @@
-const core =  require('@actions/core');
-const github = require('@actions/github');
-const exec = require('@actions/exec');
+const core = require('@actions/core');
+const fs = require('fs');
 
+const {
+  S3Client,
+  PutObjectCommand
+} = require('@aws-sdk/client-s3');
 
-function hello(){
-    core.notice("Hello from my custom javascript action!!");
+async function run() {
+
+  try {
+
+    core.notice("Starting upload to Cloudflare R2...");
+
+    const client = new S3Client({
+      region: 'auto',
+
+      endpoint: process.env.R2_ENDPOINT,
+
+      credentials: {
+        accessKeyId: process.env.R2_ACCESS_KEY_ID,
+        secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
+      },
+    });
+
+    // file you want to upload
+    const fileContent = fs.readFileSync('./index.html');
+
+    const command = new PutObjectCommand({
+      Bucket: process.env.R2_BUCKET_NAME,
+      Key: 'index.html',
+      Body: fileContent,
+      ContentType: 'text/html',
+    });
+
+    await client.send(command);
+
+    core.notice("File uploaded successfully!");
+
+  } catch (error) {
+
+    core.setFailed(error.message);
+
+  }
 }
 
-hello()
+run();
